562d6419014ad00e21520b54906b5233334295d1
Luis Franco <99168105+Blaxery@users.noreply.github.com>
2024-03-10 20:59:29 +0000

    se agregaron nuevos archivos

---
+++ app.js
@@ -0,0 +1,12 @@
+function generarCarta() {
+    var palos = ['spade', 'heart', 'diamond', 'club'];
+    var valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    
+    var paloIndex = Math.floor(Math.random() * 4);
+    var valorIndex = Math.floor(Math.random() * 13);
+    
+    var carta = document.getElementById('card');
+    carta.className = 'card ' + palos[paloIndex];
+    carta.innerHTML = valores[valorIndex];
+  }
+  
